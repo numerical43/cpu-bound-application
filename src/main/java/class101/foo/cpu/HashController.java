60bebb6399a7226e1e951166e20c8a3f6e5050d2
sui <numerical43@daum.net>
2023-01-12 18:16:56 +0900

    change 'hello API'

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello hello";
+        return "hello hello hello";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
